c6db82833528f799a01c0e646d5e3b0217f4ab02
Manuel Kiessling <manuel@kiessling.net>
2015-02-01 16:43:29 +0100

    Refactorings

--- app/controllers/Api/Experiments.scala
+++ app/controllers/Api/Experiments.scala
@@ -12,6 +12,22 @@ object Experiments extends Controller {
     if (xs.isEmpty) 0.0
     else xs.head.weight + variationsSum(xs.tail)
   }
+  
+  private def badRequest(detail: String) = {
+    BadRequest(
+      Json.toJson(
+        Map(
+          "success" -> JsBoolean(false),
+          "error" -> Json.toJson(
+            Map(
+              "message" -> "Bad Request",
+              "detail" -> detail
+            )
+          )
+        )
+      )
+    )
+  }
 
   implicit val formVariationReads: Reads[FormVariation] = (
     (JsPath \ "name").read[String] and
@@ -25,69 +41,34 @@ object Experiments extends Controller {
     )(FormExperiment.apply _)
 
   def save = Action(BodyParsers.parse.json) { request =>
+
     val formExperimentResult = request.body.validate[FormExperiment]
-    try {
-      formExperimentResult.fold(
 
-        errors => {
-          BadRequest(
-            Json.toJson(
-              Map(
-                "success" -> JsBoolean(false),
-                "error" -> Json.toJson(
-                  Map(
-                    "message" -> "Bad Request",
-                    "detail" -> "The structure of your request body could not be understood"
-                  )
-                )
-              )
-            )
-          )
-        },
+    formExperimentResult.fold(
 
-        formExperiment => {
-          if (variationsSum(formExperiment.formVariations) != 100.0) {
-            BadRequest(
-              Json.toJson(
-                Map(
-                  "success" -> JsBoolean(false),
-                  "error" -> Json.toJson(
-                    Map(
-                      "message" -> "Bad Request",
-                      "detail" -> "The sum of the variation weights must be 100.0"
-                    )
-                  )
-                )
-              )
-            )
-          } else {
-            models.Experiment.add(formExperiment) match {
-              case Some(experiment) => {
-                Ok(Json.toJson(Map("success" -> JsBoolean(true), "experimentId" -> JsNumber(experiment.id))))
-              }
-              case None => InternalServerError
+      errors => {
+        badRequest("The structure of your request body could not be understood")
+      },
+
+      formExperiment => {
+        if (variationsSum(formExperiment.formVariations) != 100.0) {
+          badRequest("The sum of the variation weights must be 100.0")
+        } else {
+          try {
+            val experimentOption = models.Experiment.add(formExperiment)
+            experimentOption.map( experiment =>
+              Ok(Json.toJson(Map("success" -> JsBoolean(true), "experimentId" -> JsNumber(experiment.id))))
+            ) getOrElse InternalServerError
+          } catch {
+            case jse: JdbcSQLException => {
+              badRequest("An experiment with this name already exists")
             }
           }
         }
-
-      )
-    } catch {
-      case jse: JdbcSQLException => {
-        BadRequest(
-          Json.toJson(
-            Map(
-              "success" -> JsBoolean(false),
-              "error" -> Json.toJson(
-                Map(
-                  "message" -> "Bad Request",
-                  "detail" -> "An experiment with this name already exists"
-                )
-              )
-            )
-          )
-        )
       }
-    }
+
+    )
+
   }
 
 }
